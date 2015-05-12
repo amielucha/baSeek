b86a0427892ba01f3bc2dd3c75356a8a611810d6
Slawomir Amielucha <slawomir@amielucha.com>
2015-05-12 13:36:57 +0200

    WooCommerce template fix

--- woocommerce.php
+++ woocommerce.php
@@ -1,12 +1,5 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
  * @package baSeek
  */
 
@@ -17,11 +10,7 @@ get_header(); ?>
 			<div id="primary" class="content-area <?php content_w() ?>">
 				<main id="main" class="site-main" role="main">
 
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php woocommerce_content(); ?>
-
-					<?php endwhile; // end of the loop. ?>
+					<?php woocommerce_content(); ?>
 
 				</main><!-- #main -->
 			</div><!-- #primary -->
