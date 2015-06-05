26de93e07e4ee28f495fcf7c9ee2ae37edcb7985
Slawomir Amielucha <slawomir@amielucha.com>
2015-06-05 16:44:51 +0200

    Plugin-specific behaviour
    Changes the default timestamps when 'A Long Time Ago' is installed

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -78,30 +78,37 @@ if ( ! function_exists( 'baseek_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function baseek_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
 
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
+	// this will only happen if the 'A Long Time Ago' plugin is active:
+	if (function_exists('time_ago')):
+		time_ago();
+	else:
 
-	$posted_on = sprintf(
-		_x( 'Posted on %s', 'post date', 'baseek' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
 
-	echo '<span class="posted-on">' . $posted_on . '</span>';
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( 'c' ) ),
+			esc_html( get_the_modified_date() )
+		);
 
-	/*$byline = sprintf(
-		_x( 'by %s', 'post author', 'baseek' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';*/
+		$posted_on = sprintf(
+			_x( 'Posted on %s', 'post date', 'baseek' ),
+			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		);
+
+		echo '<span class="posted-on">' . $posted_on . '</span>';
+
+		/*$byline = sprintf(
+			_x( 'by %s', 'post author', 'baseek' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		);
+		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';*/
+	endif;
 }
 endif;
 
