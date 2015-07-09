2e51651dd46d904fb3a21ff4bb560d9c5f3e31c8
Slawomir Amielucha <slawomir@amielucha.com>
2015-07-09 13:43:47 +0200

    Editor's permissions settings

--- functions.php
+++ functions.php
@@ -293,3 +293,75 @@ function get_topmost_parent($post_id){
     return get_topmost_parent($parent_id);
   }
 }
+
+// Gravity forms - enable hide labels dropdown
+add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+
+// Extend Editor's permissions
+function add_grav_forms(){
+    $role = get_role('editor');
+    $role->add_cap('gform_full_access');
+
+		$role->add_cap('list_users');
+		$role->add_cap('create_users');
+		$role->add_cap('delete_users');
+}
+add_action('admin_init','add_grav_forms');
+
+/*
+ * Allow Editors to add any users except Administrators
+ */
+class JPB_User_Caps {
+
+  // Add our filters
+  function JPB_User_Caps(){
+    add_filter( 'editable_roles', array(&$this, 'editable_roles'));
+    add_filter( 'map_meta_cap', array(&$this, 'map_meta_cap'),10,4);
+  }
+
+  // Remove 'Administrator' from the list of roles if the current user is not an admin
+  function editable_roles( $roles ){
+    if( isset( $roles['administrator'] ) && !current_user_can('administrator') ){
+      unset( $roles['administrator']);
+    }
+    return $roles;
+  }
+
+  // If someone is trying to edit or delete and admin and that user isn't an admin, don't allow it
+  function map_meta_cap( $caps, $cap, $user_id, $args ){
+
+    switch( $cap ){
+        case 'edit_user':
+        case 'remove_user':
+        case 'promote_user':
+            if( isset($args[0]) && $args[0] == $user_id )
+                break;
+            elseif( !isset($args[0]) )
+                $caps[] = 'do_not_allow';
+            $other = new WP_User( absint($args[0]) );
+            if( $other->has_cap( 'administrator' ) ){
+                if(!current_user_can('administrator')){
+                    $caps[] = 'do_not_allow';
+                }
+            }
+            break;
+        case 'delete_user':
+        case 'delete_users':
+            if( !isset($args[0]) )
+                break;
+            $other = new WP_User( absint($args[0]) );
+            if( $other->has_cap( 'administrator' ) ){
+                if(!current_user_can('administrator')){
+                    $caps[] = 'do_not_allow';
+                }
+            }
+            break;
+        default:
+            break;
+    }
+    return $caps;
+  }
+
+}
+
+$jpb_user_caps = new JPB_User_Caps();
